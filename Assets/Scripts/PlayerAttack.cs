9826ee6004ff72b217fae6d3e7378718065ede5d
Riley <rileykopfstein@gmail.com>
2022-12-12 19:43:09 +1030

    Made roll a little better by allowing rolls when not moving

--- Assets/Scripts/PlayerAttack.cs
+++ Assets/Scripts/PlayerAttack.cs
@@ -5,7 +5,10 @@ using UnityEngine.InputSystem;
 
 public class PlayerAttack : Attack
 {
-    [SerializeField] float rollCooldown = 0.4f;
+    [SerializeField] public float rollCooldown = 0.4f;
+    [SerializeField] public float rollSpeed = 0.3f; //Decreases alongside rollCooldown when roll speed is upgraded
+    [SerializeField] public float rollInv = 0.15f;
+    [SerializeField] float rollDist = 3f; //Constant
     [HideInInspector] public bool canRoll = true;
 
     PlayerMovement playerMovement;
@@ -36,7 +39,15 @@ public class PlayerAttack : Attack
     {
         if(context.performed && canRoll)
         {
-            GetComponent<Rigidbody2D>().velocity = GetComponent<PlayerMovement>().GetMoveVelocity() * 5;
+            if(GetComponent<Rigidbody2D>().velocity != new Vector2(0, 0))
+            {
+                GetComponent<Rigidbody2D>().velocity = GetComponent<PlayerMovement>().GetMoveVelocity().normalized * rollDist;
+            }
+            else
+            {
+                GetComponent<Rigidbody2D>().velocity = -transform.right * rollDist;
+            }
+
             StartCoroutine(RollStop());
             //Play sound
             cooldownRoutine = StartCoroutine(RollCooldown());
@@ -46,11 +57,13 @@ public class PlayerAttack : Attack
     IEnumerator RollStop()
     {
         yield return new WaitForSeconds(0.2f);
+        canAttack = true;
         GetComponent<Rigidbody2D>().velocity = GetComponent<PlayerMovement>().GetMoveVelocity();
     }
 
     IEnumerator RollCooldown()
     {
+        canAttack = false;
         canRoll = false;
         playerMovement.canMove = false;
         yield return new WaitForSeconds(rollCooldown);
