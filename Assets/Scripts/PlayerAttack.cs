fe796b5030a389e7cc02453dfaa8bd2db3c8a5ac
Riley <rileykopfstein@gmail.com>
2022-12-12 13:35:45 +1030

    Added shoot cooldown to player

--- Assets/Scripts/PlayerAttack.cs
+++ Assets/Scripts/PlayerAttack.cs
@@ -7,13 +7,23 @@ public class PlayerAttack : MonoBehaviour
 {
     [SerializeField] GameObject bullet;
     [SerializeField] Transform spawnPos;
+    [SerializeField] float shootCooldown = 0.2f;
+    [SerializeField] bool canShoot = true;
 
     public void Shoot(InputAction.CallbackContext context)
     {
-        if (context.performed)
+        if (context.performed && canShoot)
         {
             GameObject bulletInstance = Instantiate(bullet, spawnPos.position, transform.rotation);
             bulletInstance.GetComponent<Projectile>().SetDir(-transform.right);
+            StartCoroutine(ShootCooldown());
         }
+    }
+
+    IEnumerator ShootCooldown()
+    {
+        canShoot = false;
+        yield return new WaitForSeconds(shootCooldown);
+        canShoot = true;
     }
 }
