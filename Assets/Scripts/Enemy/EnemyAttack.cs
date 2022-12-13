d4f8102748a8b4f1283a3c2b485d0f1f213bd472
Logan Baker <loganpbaker05@gmail.com>
2022-12-14 01:43:37 +1030

    Big enemy deals damage!

--- Assets/Scripts/Enemy/EnemyAttack.cs
+++ Assets/Scripts/Enemy/EnemyAttack.cs
@@ -7,10 +7,23 @@ public class EnemyAttack : Attack
     float shootOffset = 0;
     [SerializeField] float meleeDelayTime = 0.4f;
 
+    [SerializeField] float superJumpCooldown = 2f;
+    [SerializeField] GameObject shockWaveParticlePrefab;
+
+    [SerializeField] float jumpAttackDamage = 20;
+
+    GameObject superJumpColliderObject;
+
     public void SetShootOffset(float _shootOffset) { shootOffset = _shootOffset; } 
     protected override void OnAwake()
     {
         base.OnAwake();
+
+        if (transform.Find("SuperJumpCollider"))
+        {
+            superJumpColliderObject = transform.Find("SuperJumpCollider").gameObject;
+        }
+        
         StartCoroutine(WaitForOffset());
     }
 
@@ -23,17 +36,21 @@ public class EnemyAttack : Attack
         {
             meleeDelay = 0;
         }
-        if (GetComponent<EnemyState>().GetState() == 1)
+        else if (GetComponent<EnemyState>().GetState() == 1)
         {
             Shoot();
         }
         else if (GetComponent<EnemyState>().GetState() == 2)
         {
-            if(meleeDelay <= 0)
+            if (meleeDelay <= 0)
             {
                 meleeDelay = meleeDelayTime;
             }
             Melee();
+        }
+        else if (GetComponent<EnemyState>().GetState() == 3)
+        {
+            SuperJump();
         }
     }
 
@@ -42,5 +59,45 @@ public class EnemyAttack : Attack
         canAttack = false;
         yield return new WaitForSeconds(shootOffset);
         canAttack = true;
+    }
+
+    void SuperJump()
+    {
+        if (canAttack)
+        {
+            transform.parent.GetComponent<Animator>().SetTrigger("Jump");
+
+            StartCoroutine(LandEnd(transform.parent.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).length));
+            StartCoroutine(SuperJumpCooldown());
+        }
+    }
+
+    IEnumerator SuperJumpCooldown()
+    {
+        canAttack = false;
+        yield return new WaitForSeconds(superJumpCooldown);
+        canAttack = true;
+    }
+
+    IEnumerator LandEnd(float _animationLength)
+    {
+        yield return new WaitForSeconds(_animationLength);
+        ParticleSystem shockwave = Instantiate(shockWaveParticlePrefab, transform.position, transform.rotation).GetComponent<ParticleSystem>();
+        shockwave.Play();
+        superJumpColliderObject.GetComponent<CircleCollider2D>().enabled = true;
+        StartCoroutine(ColliderActiveTime(0.1f));
+    }
+
+    IEnumerator ColliderActiveTime(float _secondsActive)
+    {
+        yield return new WaitForSeconds(_secondsActive);
+
+        superJumpColliderObject.GetComponent<OnSuperJumpTrigger>().SetHasHitPlayer(false);
+        superJumpColliderObject.GetComponent<CircleCollider2D>().enabled = false;
+    }
+
+    public void DealJumpDamage(GameObject _objectToHit)
+    {
+        _objectToHit.GetComponent<Health>().TakeDamage(jumpAttackDamage, this);
     }
 }
